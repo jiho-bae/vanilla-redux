d974d095c5be149dd70830ac0e8659ed6763c0ab
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-03-11 17:07:19 +0900

    modify our vanilla code to redux.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
+number.innerText = 0;
 
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  count++;
-  updateText();
+  countStore.dispatch({ type: "ADD" });
 };
-
 const handleMinus = () => {
-  count--;
-  updateText();
+  countStore.dispatch({ type: "MINUS" });
 };
 
 add.addEventListener("click", handleAdd);
